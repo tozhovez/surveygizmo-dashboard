a303006fe783c12159b7fed2fc23d4dccba6d9f0
Marin Bareta <mbareta@extensionengine.com>
2017-03-13 14:15:57 +0100

    Improve code

--- routes/responses.js
+++ routes/responses.js
@@ -1,25 +1,22 @@
-var express = require('express');
-var router = express.Router();
-var surveyGizmo = require('../lib/SurveyGizmo');
-var EdxApi = require('../lib/EdxApi');
+const express = require('express');
+const router = express.Router();
+const surveyGizmo = require('../lib/SurveyGizmo');
+const EdxApi = require('../lib/EdxApi');
 
 const config = require('../config/main');
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) =>
   surveyGizmo.getData()
-    .then(json => res.send(json))
-    .catch(error => next(error))
-});
+  .then(json => res.send(json))
+  .catch(error => next(error))
+);
 
-
-router.get('/:responseId/approve', (req, res, next) => {
+router.get('/:responseId/approve', (req, res, next) =>
   surveyGizmo.getResponse(req.params.responseId)
   .then(response => EdxApi.createAccount(response.questions))
   .then(account => EdxApi.grantCcxRole(account.username, req.session.token.access_token))
   .then(username => res.send(username))
-  .catch(error => next(error));
-});
-
-
+  .catch(error => next(error))
+);
 
 module.exports = router;
