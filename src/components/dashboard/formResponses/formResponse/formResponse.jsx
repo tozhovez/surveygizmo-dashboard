482516a63b2dd0cb0272b0cddeb764920b8ede71
Marin Bareta <mbareta@extensionengine.com>
2017-03-15 14:10:56 +0100

    Add modal for confirming approval

--- src/components/dashboard/formResponses/formResponse/formResponse.jsx
+++ src/components/dashboard/formResponses/formResponse/formResponse.jsx
@@ -6,37 +6,21 @@ module.exports = class FormResponse extends React.Component {
 
     this.state = {
       approved: false
-    }
-  }
-
-  approveResponse() {
-    const xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = () => {
-      if(xhr.readyState === 4 && xhr.status === 200) {
-        this.setState({approved: true});
-      }
-      else if (xhr.readyState === 4 && xhr.status !== 200) {
-        throw new Error('Approve response failed');
-      }
-    }
-
-    xhr.open('POST', `/responses/${this.props.response.id}/approve`, true);
-    xhr.send();
+    };
   }
 
   render() {
-    const { response } = this.props;
+    const { response, showApproveModal } = this.props;
     const { questions } = response;
 
     return (
       <tr className={`form-responses-item ${this.state.approved ? 'approved' : 'not-approved'}`}>
         <td>{`${questions['First Name']} ${questions['Last Name']}`}</td>
         <td>{questions['Email Address']}</td>
-        <td>{response.submittedAt}</td>
         <td>{questions['Company Name']}</td>
+        <td>{(new Date(response.submittedAt)).toLocaleDateString()}</td>
         <td>
-          <button onClick={() => this.approveResponse()}>Approve</button>
+          <button onClick={() => showApproveModal(response.id)}>Approve</button>
         </td>
       </tr>
     );
--- src/components/dashboard/formResponses/formResponses.jsx
+++ src/components/dashboard/formResponses/formResponses.jsx
@@ -1,33 +1,41 @@
 const React = require('react');
 const FormResponse = require('./formResponse/formResponse.jsx');
+const ApproveModal = require('../modals/approveModal/approveModal.jsx');
 
 module.exports = class FormResponses extends React.Component {
   constructor() {
     super();
 
+    this.showApproveModal = this.showApproveModal.bind(this);
+
     this.state = {
-      responses: []
-    }
+      responses: [],
+      approveResponseId: null
+    };
   }
 
   getResponses() {
     const xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = () => {
-      if(xhr.readyState === 4 && xhr.status === 200) {
-        this.setState({responses: JSON.parse(xhr.responseText)});
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        this.setState({ responses: JSON.parse(xhr.responseText) });
       }
       else if (xhr.readyState === 4 && xhr.status !== 200) {
         throw new Error('Fetching responses failed');
       }
-    }
+    };
 
     xhr.open('GET', '/responses', true);
     xhr.send();
   }
 
+  showApproveModal(approveResponseId) {
+    this.setState({ approveResponseId });
+  }
+
   componentDidMount() {
-    this.getResponses()
+    this.getResponses();
   }
 
   render() {
@@ -41,19 +49,25 @@ module.exports = class FormResponses extends React.Component {
             <tr>
               <td>Name</td>
               <td>Email</td>
-              <td>Submitted at</td>
               <td>Company Name</td>
+              <td>Submitted at</td>
               <td>Actions</td>
             </tr>
           </thead>
           <tbody>
             {
               this.state.responses.map(response =>
-                <FormResponse key={`form-response-${response.id}`} response={response} />
+                <FormResponse
+                  key={`form-response-${response.id}`}
+                  response={response}
+                  showApproveModal={this.showApproveModal}
+                />
               )
             }
           </tbody>
         </table>
+
+        <ApproveModal responseId={this.state.approveResponseId} />
       </div>
     );
   }
