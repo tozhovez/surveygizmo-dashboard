61bced0d178475b6540e4778a9c9598bf341a267
Miro Dojkic <mdojkic@gmail.com>
2017-03-20 08:06:58 +0100

    Async mongoose save and responses middleware refactor

--- routes/responses.js
+++ routes/responses.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const surveyGizmo = require('../lib/SurveyGizmo');
-const EdxApi = require('../lib/EdxApi');
-
-const SurveyResponse = require('../models/surveyResponse');
+const { approveResponse } = require('../middlewares/responses');
 
 const router = express.Router();
 
@@ -12,41 +10,6 @@ router.get('/', (req, res, next) =>
   .catch(error => next(error))
 );
 
-router.post('/:responseId/approve', (req, res, next) => {
-  const data = {
-    submittedAt: '',
-    questions: {},
-    status: {
-      accountCreated: false,
-      grantedCcxRole: false,
-      sentPasswordReset: false
-    }
-  };
-
-  surveyGizmo.getResponse(req.params.responseId)
-  .then(response => {
-    data.contents = response.questions;
-    data.submittedAt = response.submittedAt;
-
-    return EdxApi.createAccount(response.questions);
-  })
-  .then(account => {
-    data.status.accountCreated = true;
-    return EdxApi.grantCcxRole(account, req.session.token.access_token);
-  })
-  .then(account => {
-    data.status.grantedCcxRole = true;
-    return EdxApi.sendResetPasswordRequest(account);
-  })
-  .then(account => {
-    data.status.sentPasswordReset = true;
-
-    const surveyResponse = new SurveyResponse(data);
-    surveyResponse.save();
-
-    res.send(account.username);
-  })
-  .catch(error => next(error));
-});
+router.post('/:responseId/approve', approveResponse);
 
 module.exports = router;
