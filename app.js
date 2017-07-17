ab58a8e7edd0ae16df0e3196d2b254c9ce5af8b2
Miro Dojkic <mdojkic@gmail.com>
2017-07-17 10:25:01 +0200

    KF-355: Update favicon image (#51)
    * update favicon
    
    * update favicon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -23,8 +23,7 @@ app.locals.config = config;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -68,8 +67,8 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message; // eslint-disable-line no-param-reassign
+  res.locals.error = req.app.get('env') === 'development' ? err : {}; // eslint-disable-line no-param-reassign
 
   // render the error page
   res.status(err.status || 500);
